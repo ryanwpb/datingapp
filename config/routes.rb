5bdf99546a616418063b0cbda9554c0da0609842
Ryan Graham <ryangrahamwpb@gmail.com>
2016-05-02 14:24:46 -0400

    Add Sessions Controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   resources :users
 
   root to: 'users#index'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
