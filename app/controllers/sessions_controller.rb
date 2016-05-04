fddbb22833db79b777b65a8a33e39f2d2dd21d3a
Ryan Graham <ryangrahamwpb@gmail.com>
2016-05-04 12:14:50 -0400

    Set strong params.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    u = User.find_by_email(params[:email])
      render text: request.env['omniauth.auth'].to_yaml
   end
 
   def destroy
+     session[:user_id] = nil
+     session[:user_type] = nil
+     redirect_to login_path, notice: "You have been logged out!"
   end
+
+  private
+
+   def set_params
+     params.require(:session).permit(:name, :email, :password)
+   end
 end
