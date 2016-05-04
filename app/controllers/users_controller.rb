244be941a04e277561b3fe340c3a769a276736b9
Ryan Graham <ryangrahamwpb@gmail.com>
2016-05-04 13:48:57 -0400

    Add serialize to user conytroller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-
+  serialize :preferences, JSON
+  
   # GET /users
   # GET /users.json
   def index
